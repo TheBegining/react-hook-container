b8037fa3b66372d27c7e8e90acabf67cb9c135f7
cqh <the_sagacious@sina.com>
2019-06-22 13:57:13 +0800

    Add example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createContainer } from "./lib/react-hook-container";
+
+const useA = (initState: number) => {
+  const [a, setA] = useState(initState);
+  return { a, setA };
+};
+
+const useB = () => {
+  const [b, setB] = useState<number | null>(null);
+  return { b, setB };
+};
+
+const AContainer = createContainer(useA);
+
+const BContainer = createContainer(useB);
 
 const App: React.FC = () => {
   return (
@@ -21,6 +36,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
